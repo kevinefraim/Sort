0d26f11666fe6ce52e2980b6eb45e92d538f318c
Kevin Efraim <kevinefraim02@gmail.com>
2022-02-02 18:09:39 -0300

    random card final

---
+++ ej-2-randomCard/js/randomCard.js
@@ -0,0 +1,42 @@
+const cards = [];
+const createDeck = () => {
+  const values = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const palos = ["Hearts", "Spades", "Diamonds", "Clubs"];
+
+  for (let v = 0; v < values.length; v++) {
+    for (let p = 0; p < palos.length; p++) {
+      const valor = values[v];
+      const palo = palos[p];
+
+      cards.push({ valor, palo });
+    }
+  }
+
+  return cards;
+};
+console.log(createDeck());
+
+const randomCard = () => {
+  const random = Math.floor(Math.random() * 35);
+  console.log(random);
+  const cardValor = cards[random].valor;
+  const cardPalo = cards[random].palo;
+
+  let entity;
+  cardPalo === "Diamonds"
+    ? (entity = "&diams;")
+    : (entity = `&${cardPalo.toLowerCase()};`);
+
+  const cardContainer = document.querySelector("#card");
+  cardContainer.classList.add("card", cardPalo.toLowerCase());
+  cardContainer.innerHTML = `
+    <span>${entity} </span>
+    <p class="text-center">
+    ${cardValor}
+    </p>
+    <div class="d-flex justify-content-end"><span>
+    ${entity}
+   </span></div>`;
+};
+
+randomCard();
